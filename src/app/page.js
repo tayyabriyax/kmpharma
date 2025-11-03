97f9c02d12c5afc666f2adc9ed45132c738ad418
Tayyab Riaz <tayyabriyax@gmail.com>
2025-11-03 14:51:06 +0500

    Make login page as Landing Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,18 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/login");
+  }, [])
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-white font-sans">
-      
+
     </div>
   );
 }
